1be80eeadd279e34dfd5c262a08a01cccabb924d
Dawid <dawidkotlin@gmail.com>
2025-05-10 03:38:35 +0200

    .

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/poor_mans_excel',
-  // assetPrefix: '/poor_mans_excel/',
   images: {
     unoptimized: true,
   },
-  // trailingSlash: true,
-  // // Ensure all assets are properly loaded
-  // webpack: (config) => {
-  //   config.output.publicPath = '/poor_mans_excel/';
-  //   return config;
-  // },
-  /* config options here */
 };
 
 export default nextConfig;
