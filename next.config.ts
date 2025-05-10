9ca7fe626725d4c7c439168b4a959a95425dfd00
Dawid <dawidkotlin@gmail.com>
2025-05-10 03:04:55 +0200

    Configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/dawdmaow/poor_mans_excel',
+  images: {
+    unoptimized: true,
+  },
   /* config options here */
 };
 
