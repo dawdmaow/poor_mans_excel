764818a5701e5254bd34074a0b12deb74f602463
Dawid <dawidkotlin@gmail.com>
2025-05-10 03:07:48 +0200

    Fix basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/dawdmaow/poor_mans_excel',
+  basePath: '/poor_mans_excel',
   images: {
     unoptimized: true,
   },
